b55da332d94e0328a45774f7c309a191f222a25c
Heather Creighton <heather.creighton@gmail.com>
2017-10-06 09:38:01 -0400

    added code to account to for no tweets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,9 +24,13 @@ module ApplicationHelper
    end	
 
    def top_tweeter
-    user_id =  Tweet.all.group_by { |h| h['user_id']}.to_a.max_by {|x| x[1].length}.first 
-    user = User.find(user_id)
-    return user
-   end
+     tweets = Tweet.all
+    if tweets.length > 0
+      user_id =  Tweet.all.group_by { |h| h['user_id']}.to_a.max_by {|x| x[1].length}.first 
+      user = User.find(user_id)
+      return user
+    end
+   end 
+
 
 end
